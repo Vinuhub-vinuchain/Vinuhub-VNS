1e82d372475eaa52a628d1bb726c6551c81a766f
VINUHUB <aladoemeka1@gmail.com>
2026-01-11 10:41:25 -0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useWallet } from '../hooks/useWallet';
-import '../styles/Header.module.css';
 
 const Header: React.FC = () => {
   const { userAddress, connectWallet, disconnectWallet, status } = useWallet();
 
   return (
-    <header className='header'>
-      <div className='logo'>
-        <Link to="/">VinuHub .vc</Link>
+    <header>
+      <div className="logo">
+        <img src="https://photos.pinksale.finance/file/pinksale-logo-upload/1759847695513-f915ce15471ce09f03d8fbf68bc0616f.png" alt="VinuHub Logo" />
       </div>
-      <nav className='nav'>
+      <i className="fas fa-bars hamburger"></i>
+      <nav>
+        <Link to="/">Home</Link>
         <Link to="/search">Search</Link>
-        <Link to="/register">Register</Link>
         <Link to="/transfer">Transfer</Link>
         <Link to="/marketplace">Marketplace</Link>
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/history">History</Link>
       </nav>
-      <div className='wallet'>
-        {userAddress ? (
-          <>
-            <span>{`${userAddress.slice(0, 6)}...${userAddress.slice(-4)}`}</span>
-            <button onClick={disconnectWallet}>Disconnect</button>
-          </>
-        ) : (
-          <button onClick={connectWallet}>Connect Wallet</button>
-
-        )}
+      <div className="header-controls">
+        <button id="connectWallet" onClick={() => connectWallet()}>
+          <i className="fas fa-wallet"></i> Connect Wallet
+        </button>
+        <button id="disconnectWallet" style={{ display: userAddress ? 'block' : 'none' }} onClick={disconnectWallet}>
+          <i className="fas fa-sign-out-alt"></i> Disconnect Wallet
+        </button>
       </div>
-      {status && <p className='status'>{status}</p>}
     </header>
   );
 };

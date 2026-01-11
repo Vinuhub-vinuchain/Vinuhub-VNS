76d3a214e1a19b87254e70051243d21684de7d51
VINUHUB <aladoemeka1@gmail.com>
2026-01-11 10:42:42 -0800

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <p>Powered by VinuChain © 2025</p>
+      <div className="social">
+        <a href="https://t.me/Vinuhub" target="_blank">
+          <i className="fab fa-telegram"></i> Join Telegram
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

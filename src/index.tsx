7e9142515e789ad752f9e9350c8a866f33667884
VINUHUB <aladoemeka1@gmail.com>
2026-01-11 10:39:47 -0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
+// src/index.tsx
 import React from 'react';
-import ReactDOM from 'react-dom/client';            
+import ReactDOM from 'react-dom/client';
+import './styles/global.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
